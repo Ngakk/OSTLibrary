65f46c45188e4f81ea7cc044fc2f0b128ccd2a59
Ngakk <32682022+Ngakk@users.noreply.github.com>
2018-07-09 13:04:00 -0500

    layout principal, barra de guesses

--- ostguess.php
+++ ostguess.php
@@ -27,6 +27,51 @@ $pusher->trigger('private-guess-channel', 'client-guess', array('message' => 'he
 ?>
 <body>
 
+<div style="background-color: #F2F4F3; height:100%">
+<nav class="navbar navbar-default menu">
+	<div class="col-sm-2"></div>
+    <div class="col-md-8" id="navBardiv">
+        <div class="navbar-header">
+        	<a class="navbar-brand" href="#">OST Library</a>
+        </div>
+    </div>
+	<div class ="col-sm-2"></div>
+</nav>
+<!-- Game -->
+<div class="ostguessblock blocktopleft ">
+    
+</div>
+<!-- Chat -->
+<div class="ostguessblock blocktopright">
+    <div class="form-control guessChat" id="guessChat"></div>
+</div>
+<!-- Game score-->
+<div class="ostguessblock blockbottomleft">
+    
+</div>
+<!-- Chat Input -->
+<div class="ostguessblock blockbottomright">
+    <form id="">
+    	<div class="form-group insideblock">
+    		<label for="guessInput">Make a guess: </label>
+        	<input type="text" class="form-control" id="guessInput">
+            <div id="guessesDropdown" class="dropdown-content">
+              <div class="guessSource"><a href="#home">Home</a></div>
+              <div class="guessSource"><a href="#about">About</a></div>
+              <div class="guessSource"><a href="#contact">Contact</a></div>
+              <div class="guessSource"><a href="#1">Killer Instinct</a></div>
+              <div class="guessSource"><a href="#2">Donkey Kong Country</a></div>
+              <div class="guessSource"><a href="#3">Celeste</a></div>
+              <div class="guessSource"><a href="#4">My Hero Academia</a></div>
+              <div class="guessSource"><a href="#5">Cowboy Bebop</a></div>
+            </div>
+        </div>
+    </form>
+</div>
+
+</div>
+
+
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
 <script src="https://js.pusher.com/4.1/pusher.min.js"></script>
 <script>
@@ -49,8 +94,57 @@ $pusher->trigger('private-guess-channel', 'client-guess', array('message' => 'he
 	channel.bind('client-globalresponse', function(data) {
 	  alert(JSON.stringify(data));
 	}); 
-
-
+	
+	$(document).ready(function(event){
+		$("#guessInput").on("keyup", function(){
+			if($(this).val().length >= 3){
+				$("#guessesDropdown").addClass("show");
+				guessFilterFunction();
+			}
+			else{
+				$("#guessesDropdown").removeClass("show");
+			}
+		});
+	});
+	
+	$("#guessForm").submit(function(event) {
+    	event.preventDefault();
+		makeAGuess(1);
+	});
+	
+	function makeAGuess(id){//Recive el id de el source que va a adivinar
+		//TODO: manda un ajax a un php que checa si la respuesta es correcta y les responde a todos en la sala
+	}
+	
+	function guessFilterFunction() {
+		/*var input, filter, ul, li, a, i;
+		input = document.getElementById("guessInput");
+		filter = input.value.toUpperCase();
+		div = document.getElementById("guessesDropdown");
+		a = div.getElementsByTagName("a");
+		for (i = 0; i < a.length; i++) {
+			if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+				a[i].style.display = "";
+			} else {
+				a[i].style.display = "none";
+			}
+		}*/
+		
+		var input, filter, ul, li, a, i;
+		input = document.getElementById("guessInput");
+		filter = input.value.toUpperCase();
+		div = document.getElementById("guessesDropdown");
+		source = div.getElementsByClassName("guessSource");
+		//TODO: acomodar a que jale con los divs de source
+		for (i = 0; i < a.length; i++) {
+			if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+				a[i].style.display = "";
+			} else {
+				a[i].style.display = "none";
+			}
+		}
+		
+	}
 </script>
 </body>
 
